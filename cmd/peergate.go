3a0d0fb6f5b49e5aef48b2b57c5330455638d375
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2025-02-21 20:22:32 +0530

    added the peergate and proxy with the util module

---
+++ cmd/peergate.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/thealonemusk/WarpNet/pkg/trustzone/authprovider/ecdsa"
+	"github.com/urfave/cli/v2"
+)
+
+func Peergate() *cli.Command {
+	return &cli.Command{
+		Name:        "peergater",
+		Usage:       "peergater ecdsa-genkey",
+		Description: `Peergater auth utilities`,
+		Subcommands: cli.Commands{
+			{
+				Name: "ecdsa-genkey",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name: "privkey",
+					},
+					&cli.BoolFlag{
+						Name: "pubkey",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					priv, pub, err := ecdsa.GenerateKeys()
+					if !c.Bool("privkey") && !c.Bool("pubkey") {
+						fmt.Printf("Private key: %s\n", string(priv))
+						fmt.Printf("Public key: %s\n", string(pub))
+					} else if c.Bool("privkey") {
+						fmt.Printf(string(priv))
+					} else if c.Bool("pubkey") {
+						fmt.Printf(string(pub))
+					}
+					return err
+				},
+			},
+		},
+	}
+}
